aa73aead218362230104c59afb7b7ac6c04f7bb2
U-momon-PC\momon <muhlisbc@gmail.com>
2017-09-20 11:13:12 +0700

    v 0.3

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-pm-scanner
 # about: Discourse plugin that scan PM's with specific keywords and will notify admin if find any
 # authors: Muhlis Budi Cahyono (muhlisbc@gmail.com)
-# version: 0.2
+# version: 0.3
 # url: http://git.dev.abylina.com/momon/discourse-pm-scanner
 
 enabled_site_setting :pm_scanner_enabled
@@ -21,12 +21,12 @@ after_initialize {
   
           if match_data = self.raw.match(regexp) # nil or MatchData
   
-            admins = User.where(admin: true).pluck(:id) # collect admin ids
-            users  = post_topic.topic_users.pluck(:user_id) # collect users
+            admin_ids = User.where("id > ?", 0).where(admin: true).pluck(:id)
+            user_ids  = post_topic.topic_allowed_users.pluck(:user_id)
   
-            if (admins & users).empty? # if admins are not in the conversation
+            if (admin_ids & user_ids).empty? # if admins are not in the conversation
 
-              admins.select{ |adm_id| adm_id > 0 }.each do |adm_id| # notify ONLY human admins
+              admin_ids.each do |adm_id| # notify ONLY human admins
                 notif_payload = {topic_id: post_topic.id, user_id: adm_id, post_number: self.post_number, notification_type: Notification.types[:custom]}
                 if Notification.where(notif_payload).first.blank?
                   Notification.create(notif_payload.merge(data: {message: "pm_scanner.notification.found", display_username: match_data.to_s, topic_title: post_topic.title}.to_json))
